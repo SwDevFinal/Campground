71fbff7158de1e85af62b7a30532c88375c43a37
Kittipong <kittipong.deevee@gmail.com>
2022-04-14 01:00:34 +0700

    Implement a simple logout method.
    TODO: Add auth middleware with protect method before accessing this route.

--- controllers/auth.js
+++ controllers/auth.js
@@ -93,3 +93,19 @@ exports.login = async (req, res, next) => {
     });
   }
 };
+
+/*
+ *  @desc   Log user out / clear cookie.
+ *  @route  GET /auth/logout
+ *  @access Private
+ */
+exports.logout = async (req, res, next) => {
+  res.cookie("token", "none", {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true,
+  });
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+};
