6efdddf434bd1674d0d8afe0ef57b6a8949f5956
Kittipong <kittipong.deevee@gmail.com>
2022-04-14 00:41:52 +0700

    Implement a login method.
    Login can be performed by POST /auth/login.

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,3 +48,48 @@ exports.register = async (req, res, next) => {
     console.log(err.stack);
   }
 };
+
+/*
+ *  @desc   Login user
+ *  @route  POST /login
+ *  @access Public
+ */
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    //   Validate email and password.
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ success: false, msg: "Please provide an email and password." });
+    }
+
+    //   Check for user
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        msg: "Login unsuccessful, please check your email and password.",
+      });
+    }
+
+    //   Check if password matches.
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        msg: "Login unsuccessful, please check your email and password.",
+      });
+    }
+
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      msg: "Login unsuccessful, please check your email and password.",
+    });
+  }
+};
