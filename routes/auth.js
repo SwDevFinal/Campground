634d48607ddebee87a41f3b2caf3a21fbc757070
Kittipong <kittipong.deevee@gmail.com>
2022-04-14 01:17:12 +0700

    Implement protect in auth middleware and add it to logout.

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const { register, login, logout } = require("../controllers/auth");
+const { protect } = require("../middleware/auth");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/logout", logout);
+router.get("/logout", protect, logout);
 
 module.exports = router;
